bce9307373c29b432197304a0cd00fadfbf01627
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-08-10 00:12:54 -0500

    another-example

--- iterview 1/index.js
+++ iterview 1/index.js
@@ -7,3 +7,34 @@ p.hello()
 Object.freeze(p)
 p.name='pap8'
 p.hello()
+
+const citiesList =[
+  'nashville',
+  'nashville',
+  'Los Angeles',
+  'nashville',
+  'menphis',
+  'barcelona',
+  'Los Angeles',
+  'Sevilla',
+  'Madrid',
+  'canary island',
+  'barcelona',
+  'london ',
+  'Berlin',
+  'Madrid'
+
+];
+const MosCitiesR=  (numCities) => {
+    const cities = {}
+     citiesList.forEach(city => {
+        cities[city]= !cities[city]? 1 : cities[city] +1 
+    })
+  return Object.keys(cities)
+  .map(city => 
+  ({name: city, times: cities[city]}))
+  .sort((a,b)=> b.times - a.times)
+  .slice(0, numCities)
+  .map(city => city.name)
+}
+console.log(MosCitiesR(3))
